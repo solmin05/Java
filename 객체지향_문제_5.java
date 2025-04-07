0ba176396139856deec97f0b7a52f080025b92b1
solmin05 <166412164+solmin05@users.noreply.github.com>
2025-04-07 22:48:28 +0900

    Create 객체지향_문제_5.java

---
+++ 객체지향_문제_5.java
@@ -0,0 +1,39 @@
+/*[ 예시문제 ] 
+사용자에게서 총 9개의 숫자를 입력 받아, 
+3 X 3 배열을 초기화 하시오. 
+3 X 3 배열에서 대각선 원소들의 합을 출력하시오.
+
+입력예시1
+1 2 3
+4 5 6
+7 8 9
+
+출력예시1
+15
+
+(위 예에서 대각선은 1, 5, 9 이고 그 합은 15 이다)
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        int sum = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            sum += arr[i][i]; 
+        }
+
+        
+        System.out.println(sum);
+
+        scanner.close();
+    }
+}
