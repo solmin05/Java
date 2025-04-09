b63606b091a5d8a6bff59d3204bb4173f2d51024
solmin05 <166412164+solmin05@users.noreply.github.com>
2025-04-09 20:21:02 +0900

    Create 객체지향_문제_4.java

--- 객체지향_문제_4.java
+++ 객체지향_문제_4.java
@@ -17,34 +17,26 @@
 5
 */
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] nums = new int[5];
-
-        // 5개 숫자 입력 받기
-        for (int i = 0; i < 5; i++) {
-            nums[i] = scanner.nextInt();
-        }
-
-        // 버블 정렬 (N-1번 반복)
-        for (int i = 0; i < 4; i++) { // N-1번 반복
-            for (int j = 0; j < 4 - i; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    // swap
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
-        }
-
-        // 정렬된 결과 출력
-        for (int num : nums) {
-            System.out.println(num);
-        }
-
-        scanner.close();
+    public class Main{
+	    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int[] nums = new int[5];
+	        for(int i=0;i<5;i++){
+	            nums[i] = sc.nextInt();
+	        }
+	        for(int i=0;i<4;i++){
+	            for(int j=0;j<4-i;j++){
+	                if(nums[j] > nums[j+1]){
+	                    int temp = nums[j];
+	                    nums[j] = nums[j+1];
+	                    nums[j+1] = temp;
+	               }
+	    }
+	    
+	}
+	for(int num : nums){
+	       System.out.println(num);
+	    }
+	    sc.close();
     }
 }
