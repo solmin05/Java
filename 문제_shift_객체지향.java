84753ae9250d6ab0947b3aed68d64334a9e062da
solmin05 <166412164+solmin05@users.noreply.github.com>
2025-04-23 10:24:21 +0900

    Create 문제_shift_객체지향.java

--- 문제_shift_객체지향.java
+++ 문제_shift_객체지향.java
@@ -1,39 +1,37 @@
 /*
 [ 문제 ] 
-영어 문자 다섯 개를 입력 받아서,  
-예시와 같이 한 번에 한 칸씩 shift해서 
-출력하는 프로그램을 작성하시오.
+배열에 10개의 숫자를 입력 받아서 저장하시오.
+  - 숫자는 1 혹은 2 혹은 3 중의 하나이다.
+  - 각 숫자의 개수 만큼 별표를 출력하시오.
 
 입력예시1
-*abcd
+1 1 1 2 2 2 3 3 3 3
 
 출력예시1
-*abcd
-d*abc
-cd*ab
-bcd*a
-abcd*
+1:***
+2:***
+3:****
 */
 import java.util.*;
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-	    char arr[] = new char[5]; //배열 5개 선언 
-	    String str = sc.next(); //문자열 받는거 
-	    
-	    for(int i=0;i<5;i++){
-	        arr[i] = str.charAt(i);
-	    }
-        // 입력받은 문자열을 한 칸씩 오른쪽으로 shift하여 출력
-        for (int i = 5; i > 0; i--) {
-            // 한 칸씩 오른쪽으로 shift하여 출력
-            for (int j = 0; j < 5; j++) {
-                int newIndex = (j + i) % 5; // 오른쪽으로 한 칸씩 shift한 인덱스 계산
-                System.out.print(arr[newIndex]);
-            }
-            System.out.println(); // 다음 줄로 이동
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10]; // 입력 받을 숫자들을 저장할 배열 생성
+
+        // 10개의 숫자 입력 받기
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        scanner.close(); // Scanner 객체 닫기
+        // 숫자별로 '*' 출력
+        for (int num = 1; num <= 3; num++) { // 입력 범위가 1부터 3까지로 가정
+            System.out.print(num + ": "); 
+            for (int i = 0; i < 10; i++) { 
+                if (arr[i] == num) { 
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
     }
 }
